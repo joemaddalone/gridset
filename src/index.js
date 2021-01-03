33788ee5f67b64db1605b4604fb245ef3af906f2
Joe Maddalone <>
2021-01-03 13:38:20 -0600

    move generators into class

--- src/index.js
+++ src/index.js
@@ -1,70 +1,3 @@
-function* scan(arr, si) {
-  let dir = 'f';
-  let index = si ?? -1;
-  const end = arr.length - 1;
-  while (true) {
-    switch (dir) {
-      case 'f':
-        if (index !== end) {
-          index++;
-        }
-        if (index === end) {
-          dir = 'r';
-        }
-        break;
-      case 'r':
-        if (index !== 0) {
-          index--;
-        }
-        if (index === 0) {
-          dir = 'f';
-        }
-        break;
-    }
-    yield arr[index];
-  }
-}
-
-function* bounce(arr, sx, sy, initMx = 1, initMy = 1) {
-  let mx = initMx;
-  let my = initMy;
-  // // Because the first yield adds mx/my we reduce sx/sy by mx/my.
-  let x = sx ? sx - 1 : -1;
-  let y = sy ? sy - 1 : -1;
-  const w = arr.length - 1;
-  const h = arr[0].length - 1;
-  while (true) {
-    if (mx + x > w || mx + x < 0) {
-      mx *= -1;
-    }
-    if (my + y > h || my + y < 0) {
-      my *= -1;
-    }
-    x = x + mx;
-    y = y + my;
-    yield arr[x][y];
-  }
-}
-
-function* cycle(arr, d = 'f', si) {
-  let index = si ? si - 1 : -1;
-  let dir = d || 'f';
-  const w = arr.length - 1;
-  while (true) {
-    if (dir === 'f') {
-      if (index === w) {
-        index = -1;
-      }
-      index++;
-    } else {
-      if (index <= 0) {
-        index = w + 1;
-      }
-      index--;
-    }
-    yield arr[index];
-  }
-}
 export class Gridset {
   constructor({
     width = 0,
@@ -125,10 +58,6 @@ export class Gridset {
 
     return cellProps;
   }
-  /**
-   *
-   * @param {*} cell
-   */
   look(cell) {
     const { ci, ri } = cell;
     const make = (mode, dir) => {
@@ -335,15 +264,15 @@ export class Gridset {
       cycleDir = dir.startsWith('r') ? 'f' : 'r';
     }
 
-    return cycle(cells, cycleDir, si).next().value;
+    return this.cycler(cells, cycleDir, si).next().value;
   }
   scanCells(cells, dir = 'f', si = null) {
     cells = cells || this.flatCells;
-    return scan(cells, dir, si);
+    return this.scanner(cells, dir, si);
   }
   cycleCells(cells, dir = 'f', si = null) {
     cells = cells || this.flatCells;
-    return cycle(cells, dir, si);
+    return this.cycler(cells, dir, si);
   }
   scanRow(ri, dir = 'f', si = null) {
     const cells = this.rowCells(ri);
@@ -387,6 +316,71 @@ export class Gridset {
   }
   bounce(area, sx, sy, mx, my) {
     const cells = area || this.cells;
-    return bounce(cells, sx, sy, mx, my);
+    return this.bouncer(cells, sx, sy, mx, my);
+  }
+  *scanner(arr, si) {
+    let dir = 'f';
+    let index = si ?? -1;
+    const end = arr.length - 1;
+    while (true) {
+      switch (dir) {
+        case 'f':
+          if (index !== end) {
+            index++;
+          }
+          if (index === end) {
+            dir = 'r';
+          }
+          break;
+        case 'r':
+          if (index !== 0) {
+            index--;
+          }
+          if (index === 0) {
+            dir = 'f';
+          }
+          break;
+      }
+      yield arr[index];
+    }
+  }
+  *bouncer(arr, sx, sy, initMx = 1, initMy = 1) {
+    let mx = initMx;
+    let my = initMy;
+    // // Because the first yield adds mx/my we reduce sx/sy by mx/my.
+    let x = sx ? sx - 1 : -1;
+    let y = sy ? sy - 1 : -1;
+    const w = arr.length - 1;
+    const h = arr[0].length - 1;
+    while (true) {
+      if (mx + x > w || mx + x < 0) {
+        mx *= -1;
+      }
+      if (my + y > h || my + y < 0) {
+        my *= -1;
+      }
+      x = x + mx;
+      y = y + my;
+      yield arr[x][y];
+    }
+  }
+  *cycler(arr, d = 'f', si) {
+    let index = si ? si - 1 : -1;
+    let dir = d || 'f';
+    const w = arr.length - 1;
+    while (true) {
+      if (dir === 'f') {
+        if (index === w) {
+          index = -1;
+        }
+        index++;
+      } else {
+        if (index <= 0) {
+          index = w + 1;
+        }
+        index--;
+      }
+      yield arr[index];
+    }
   }
 }
