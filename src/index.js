76a486aced3851d3609c592e4fdba6bb72ac2c85
Joe Maddalone <>
2021-01-09 13:38:56 -0600

    Cell class

--- src/index.js
+++ src/index.js
@@ -18,45 +18,17 @@ export class Gridset {
     this.cellHeight = cellHeight;
     this.create();
   }
-  __calcCustom(rici, wh) {
-    const gridDimension = wh === 'w' ? this.width : this.height;
-    const cellDimension = wh === 'w' ? this.cellWidth : this.cellHeight;
-    const iterableDimension = wh === 'w' ? this.colCount : this.rowCount;
-    return rici * ((gridDimension - cellDimension) / (iterableDimension - 1));
-  }
   __createCell(ci, ri) {
-    const x = ci * this.gWidth;
-    const y = ri * this.gHeight;
-    const w = this.gWidth;
-    const h = this.gHeight;
     const cellProps = {
-      x,
-      y,
-      w,
-      h,
-      t: y,
-      l: x,
-      ri,
       ci,
+      ri,
+      grid: { ...this.properties },
+      w: this.cellWidth || this.gWidth,
+      h: this.cellHeight || this.gHeight,
     };
-
     cellProps.look = this.look({ ...cellProps });
 
-    if (this.cellWidth) {
-      cellProps.x = this.__calcCustom(ci, 'w');
-      cellProps.w = this.cellWidth;
-    }
-    if (this.cellHeight) {
-      cellProps.y = this.__calcCustom(ri, 'h');
-      cellProps.h = this.cellHeight;
-    }
-
-    cellProps.r = cellProps.x + cellProps.w;
-    cellProps.b = cellProps.y + cellProps.h;
-    cellProps.cx = cellProps.x + cellProps.w / 2;
-    cellProps.cy = cellProps.y + cellProps.h / 2;
-
-    return cellProps;
+    return new Cell(cellProps);
   }
   look(cell) {
     const { ci, ri } = cell;
@@ -96,7 +68,7 @@ export class Gridset {
   }
   get cells() {
     // filter null -> null = deleted cell functionality not yet implemented.
-    return this.gridMap.map((c) => c.filter((r) => r !== null));
+    return this.gridMap; // .map((c) => c.filter((r) => r !== null));
   }
   get flatCells() {
     return this.cells.flat(Infinity);
@@ -382,5 +354,70 @@ export class Gridset {
       }
       yield arr[index];
     }
+  }
+}
+
+class Cell {
+  constructor({ ci, ri, w, h, grid, look }) {
+    this.ci = ci;
+    this.ri = ri;
+    this.w = w;
+    this.h = h;
+    this.grid = grid;
+    this.look = look;
+  }
+  __calcXy(rici, wh) {
+    const gridDimension = wh === 'w' ? this.grid.width : this.grid.height;
+    const cellDimension = wh === 'w' ? this.w : this.h;
+    const iterableDimension = wh === 'w' ? this.grid.cols : this.grid.rows;
+    return rici * ((gridDimension - cellDimension) / (iterableDimension - 1));
+  }
+  get x() {
+    const val = this.__calcXy(this.ci, 'w');
+    Object.defineProperty(this, 'x', {
+      value: val,
+    });
+    return val;
+  }
+  get y() {
+    const val = this.__calcXy(this.ri, 'h');
+    Object.defineProperty(this, 'y', {
+      value: val,
+    });
+    return val;
+  }
+  get t() {
+    return this.y;
+  }
+  get r() {
+    const val = this.x + this.w;
+    Object.defineProperty(this, 'r', {
+      value: val,
+    });
+    return val;
+  }
+  get l() {
+    return this.x;
+  }
+  get b() {
+    const val = this.y + this.h;
+    Object.defineProperty(this, 'b', {
+      value: val,
+    });
+    return val;
+  }
+  get cx() {
+    const val = this.x + this.w / 2;
+    Object.defineProperty(this, 'cx', {
+      value: val,
+    });
+    return val;
+  }
+  get cy() {
+    const val = this.y + this.h / 2;
+    Object.defineProperty(this, 'cy', {
+      value: val,
+    });
+    return val;
   }
 }
