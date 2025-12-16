619b34ae8237ba03be55bfd4735855283aba6535
Joe Maddalone <joe.maddalone@gmail.com>
2025-12-16 12:14:06 -0600

    refactor: browser compat

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import globals from 'rollup-plugin-node-globals';
+import builtins from 'rollup-plugin-node-builtins';
+
 export default {
 	input: 'index.js',
 	output: [
@@ -8,6 +13,18 @@ export default {
 		{
 			file: 'dist/bundle.esm.js',
 			format: 'es'
+		},
+		{
+			file: 'dist/bundle.js',
+			format: 'iife',
+			name: 'gridset'
 		}
+	],
+	plugins: [
+		nodeResolve({ preferBuiltins: false }), // or `true`
+		commonjs(),
+		globals(),
+		builtins()
 	]
+
 };
